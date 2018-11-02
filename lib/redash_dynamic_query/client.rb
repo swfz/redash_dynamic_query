88342a1a727362b041f8445b2d677793f6689303
swfz <sawafuji.09@gmail.com>
2018-11-02 05:00:03 +0000

    error handling

--- lib/redash_dynamic_query/client.rb
+++ lib/redash_dynamic_query/client.rb
@@ -2,18 +2,23 @@ require 'net/https'
 require 'json'
 module RedashDynamicQuery
   class Client
-    attr_reader :api_key, :endpoint, :wait_interval
+    attr_reader :api_key, :endpoint, :interval, :timeout
 
-    def initialize(user_apikey:, endpoint:, wait_interval: 0.1)
-      @api_key = user_apikey
+    def initialize(user_apikey:, endpoint:, interval: 0.1, timeout: 10)
+      @api_key  = user_apikey
       @endpoint = endpoint
-      @wait_interval = wait_interval
+      @interval = interval
+      @timeout  = timeout
     end
 
     def query(query_id:, params:)
       job_id = post_job_request(query_id: query_id, params: params)
 
+      raise 'job_id not fuond.' if job_id.nil?
+
       query_result_id = poll_job(job_id: job_id)
+
+      raise 'query_result_id not fuond.' if query_result_id.nil?
 
       get_query_result(query_result_id: query_result_id)
     end
@@ -21,30 +26,65 @@ module RedashDynamicQuery
     private
 
     def post_job_request(query_id:, params:)
-      query_string = URI.encode_www_form(params.transform_keys { |k| "p_#{k}" })
-      url = "#{endpoint}/api/queries/#{query_id}/refresh?api_key=#{api_key}&#{query_string}"
-      res = Net::HTTP.post_form(URI.parse(url), {})
+      res = with_error_handling do
+        Net::HTTP.post_form(
+          post_job_url(id: query_id, queries: query_string(params: params)),
+          {}
+        )
+      end
+
       json = JSON.parse(res.body)
-      json.dig('job','id')
+      json.dig('job', 'id')
+    end
+
+    def poll_job(job_id:, count: 0)
+      res = with_error_handling do
+        Net::HTTP.get(poll_job_url(id: job_id))
+      end
+      id = JSON.parse(res).dig('job', 'query_result_id')
+
+      return id unless id.nil?
+
+      raise "wait timeout with #{timeout} seconds." if reach_limit?(count)
+
+      sleep interval
+      poll_job(job_id: job_id, count: count + 1)
     end
 
     def get_query_result(query_result_id:)
-      url = "#{endpoint}/api/query_results/#{query_result_id}.json?api_key=#{api_key}"
-      res = Net::HTTP.get(URI.parse(url))
+      res = with_error_handling do
+        Net::HTTP.get(query_result_url(id: query_result_id))
+      end
+
       JSON.parse(res)
     end
 
-    def poll_job(job_id:)
-      url = "#{endpoint}/api/jobs/#{job_id}?api_key=#{api_key}"
-      res = Net::HTTP.get(URI.parse(url))
-      id = JSON.parse(res).dig('job', 'query_result_id')
+    def post_job_url(id:, queries:)
+      URI.parse(
+        "#{endpoint}/api/queries/#{id}/refresh?api_key=#{api_key}&#{queries}"
+      )
+    end
 
-      if id.nil?
-        sleep wait_interval
-        poll_job(job_id: job_id)
-      else
-        id
-      end
+    def poll_job_url(id:)
+      URI.parse("#{endpoint}/api/jobs/#{id}?api_key=#{api_key}")
+    end
+
+    def query_result_url(id:)
+      URI.parse("#{endpoint}/api/query_results/#{id}.json?api_key=#{api_key}")
+    end
+
+    def query_string(params:)
+      URI.encode_www_form(params.transform_keys { |k| "p_#{k}" })
+    end
+
+    def reach_limit?(count)
+      count * interval > timeout
+    end
+
+    def with_error_handling
+      res = yield
+      raise res.body if res.is_a? Net::HTTPClientError
+      res
     end
   end
 end
